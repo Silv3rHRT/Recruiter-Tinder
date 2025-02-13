b611bb2d238bed23a4ed7220e921d50e40db7128
Silv3rHRT <somethingcuteandgay@gmail.com>
2025-02-13 15:21:18 -0500

    back end created, front end to display data still needed

--- src/interfaces/Candidate.interface.tsx
+++ src/interfaces/Candidate.interface.tsx
@@ -1,1 +1,8 @@
-// TODO: Create an interface for the Candidate objects returned by the API
+// Created an interface for the Candidate objects returned by the API
+export interface Candidate {
+    name: string;
+    location?: string;
+    email?: string;
+    company?: string;
+    bio?: string;
+}
--- src/pages/CandidateSearch.tsx
+++ src/pages/CandidateSearch.tsx
@@ -1,7 +1,17 @@
 import { useState, useEffect } from 'react';
 import { searchGithub, searchGithubUser } from '../api/API';
+import { Candidate } from '../interfaces/Candidate.interface';
 
 const CandidateSearch = () => {
+  const [Candidate, setCandidate] = useState<Candidate>({
+    name: '',
+    location: '', 
+    email: '',
+    company: '',
+    bio: '',
+  });
+
+  
   return <h1>CandidateSearch</h1>;
 };
 
